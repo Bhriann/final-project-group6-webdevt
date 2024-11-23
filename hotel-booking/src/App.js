a37d1d8e1b3d75413e498179525f48d26cb40d0f
Josh <josh_gareth_yao@dlsl.edu.ph>
2024-11-23 22:51:03 +0800

    Adjusted the navbar and added rooms page and routing

--- hotel-booking/src/App.js
+++ hotel-booking/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/Homepage';
+import Rooms from './components/Rooms';
+import CustomNavbar from './components/Navbar';
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+
+<CustomNavbar /> 
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/rooms" element={<Rooms />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
--- hotel-booking/src/components/Homepage.js
+++ hotel-booking/src/components/Homepage.js
@@ -1,28 +1,12 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+
 import '../style/HomePage.css';
 
 const HomePage = () => {
   return (
     <div>
-      {/* Navbar */}
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-        <Container>
-          <Navbar.Brand href="#">SuiteSpot</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#rooms">Rooms</Nav.Link>
-              <Nav.Link href="#contact">Contact</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-
       {/* Banner */}
       <div className="banner">
-        {/* Add your banner image or text */}
       </div>
 
       {/* Description Section */}
