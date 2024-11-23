8af118c7172d842f67cab1fab9fc3bdb88731218
Josh <josh_gareth_yao@dlsl.edu.ph>
2024-11-23 22:27:18 +0800

    Changed the navbar using react-bootstrap

--- hotel-booking/src/components/Homepage.js
+++ hotel-booking/src/components/Homepage.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import '../style/HomePage.css'; // Adjust the path as necessary
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import '../style/HomePage.css';
 
 const HomePage = () => {
   return (
     <div>
       {/* Navbar */}
-      <nav className="navbar">
-        <ul className="nav-list">
-          <li className="nav-item"><a href="#" className="nav-link">Home</a></li>
-          <li className="nav-item"><a href="#" className="nav-link">Rooms</a></li>
-          <li className="nav-item"><a href="#" className="nav-link">Contact</a></li>
-        </ul>
-      </nav>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="#">SuiteSpot</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#rooms">Rooms</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
       {/* Banner */}
       <div className="banner">
-        <h1 className="banner-text">Welcome to Our Hotel</h1>
+        {/* Add your banner image or text */}
       </div>
 
       {/* Description Section */}
       <div className="container">
-        <h2>About Us</h2>
+        <h2>Welcome to SuiteSpot</h2>
         <p>
           Experience luxury and comfort at our hotel. We offer a variety of rooms and top-notch
           services to make your stay unforgettable. Book with us today and enjoy a seamless experience!
